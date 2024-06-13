0ecd8bf8c1bb2da877be28e076a59b944f5d1f56
Jesse Olsson <swellbastion@gmail.com>
2024-06-13 16:19:33 -0400

    do more than one thing

--- alang.mjs
+++ alang.mjs
@@ -14,17 +14,46 @@ const builtins =
       thing = thing[args[i]];
     return thing;
   },
+  "setProperty": function(args)
+  {
+    let path = args.slice(0, -1);
+    let value = args[args.length - 1];
+    let thing = path[0];
+    for (let i = 1; i < path.length; i++)
+      thing = thing[path[i]];
+    thing = value;
+  },
   "call": function(args)
   {
     args[0].apply(null, args.slice(1));
   },
+  "function": function(args)
+  {
+    const body = `executeList(${JSON.stringify(args[1])})`;
+    return new Function(args[0], body);
+  },
+  "doThings": function(args)
+  {
+    for (const list of args[0]) executeList(list);
+  }
 };
 
 function executeList(list)
 {
   for (let i = 0; i < list.length; i++)
+  {
     if (Array.isArray(list[i]))
-      list[i] = executeList(list[i]);
+    {
+      if (list[i][0] === "list")
+      {
+        list[i] = list[i].slice(1);
+      }
+      else
+      {
+        list[i] = executeList(list[i]);
+      }
+    }
+  }
   const [first, ...others] = list;
   return builtins[first](others);
 };
